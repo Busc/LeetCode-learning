d96da6729b110742246f833e954e62b2fe11d12f
Busc <762521772@qq.com>
2017-07-28 00:33:45 +0800

    二叉树前序遍历-非递归

---
+++ 二叉树的遍历/144 Binary Tree Preorder Traversal.py
@@ -0,0 +1,51 @@
+'''
+Tree -- Medium
+
+Given a binary tree, return the preorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+'''
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :param root: TreeNode
+        :return: List[int]
+        """
+        travResult = []
+        stack = []
+        while root or stack:
+            if not root:
+                root = stack.pop()
+            travResult.append(root.val)
+            # save all 'right'
+            if root.right:
+                stack.append(root.right)
+            root = root.left
+        return travResult
+
+if __name__ == "__main__":
+    # new node
+    node1 = TreeNode(1)
+    node2 = TreeNode(2)
+    node3 = TreeNode(3)
+    # build connection
+    node1.right = node2
+    node2.left = node3
+    # assert Solution().inorderTraversal(node1) == [1, 3, 2]
+    print(Solution().preorderTraversal(node1))
