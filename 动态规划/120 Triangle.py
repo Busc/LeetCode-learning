2bac1ada3dd53e63b85d4a351c5288b907e6c48d
Busc <762521772@qq.com>
2017-07-28 10:11:35 +0800

    数塔

---
+++ 动态规划/120 Triangle.py
@@ -0,0 +1,41 @@
+'''
+Medium
+
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Note:
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+'''
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :param triangle: List[List[int]]
+        :return: int
+        """
+        if not triangle:
+            return 0
+        length = len(triangle)
+        dp = triangle[-1]
+        for i in range(length-2, -1, -1):
+            for j in range(i+1):
+                dp[j] = triangle[i][j]+min(dp[j], dp[j+1])
+        return dp[0]
+
+if __name__ == "__main__":
+    triangle = [
+                [2],
+                [3, 4],
+                [6, 5, 7],
+                [4, 1, 8, 3]
+               ]
+    print(Solution().minimumTotal(triangle))
