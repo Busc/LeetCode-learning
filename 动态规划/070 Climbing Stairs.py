7bfaeb839c374ad50408b4fee4af34d83fe9344b
Busc <762521772@qq.com>
2017-07-30 11:58:59 +0800

    不同方法爬楼梯(每步1/2阶)

---
+++ 动态规划/070 Climbing Stairs.py
@@ -0,0 +1,28 @@
+'''
+Easy
+
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+'''
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :param n:int
+        :return:int
+        """
+        if n <= 2:
+            return n
+        dp = [0 for __ in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n-1]
+
+if __name__ == "__main__":
+    # assert Solution().climbStairs(6) == 13
+    print(Solution().climbStairs(6))
