66f1477f88d58dd7c47312033a018c69e857148f
Busc <busc829@foxmail.com>
2018-04-03 16:19:52 +0800

    在有序二维数组中查找元素(行递增+列递增)

---
+++ 074 Search a 2D Matrix.py
@@ -0,0 +1,30 @@
+'''
+Array -- Medium
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+  1) Integers in each row are sorted from left to right.
+  2) The first integer of each row is greater than the last integer of the previous row.
+'''
+
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        # edge case：matrix is empty
+        if matrix:
+            rowNum = len(matrix)
+            colNum = len(matrix[0])
+            row = 0
+            col = colNum - 1
+            while row < rowNum and col >= 0:
+                if target == matrix[row][col]:
+                    return True
+                elif target > matrix[row][col]:
+                    row += 1
+                else:
+                    col -= 1
+        return False
